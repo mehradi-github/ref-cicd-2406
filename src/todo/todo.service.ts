4ec9ee1394d892e850f939df2c88c9c6ad9a1a7c
Mehradi <mehradi.info@gmail.com>
2024-06-18 09:34:38 +0200

    Adding CreateTodo in TodoService via PrismaService

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoInput } from './dto/create-todo.input';
 import { UpdateTodoInput } from './dto/update-todo.input';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TodoService {
-  create(createTodoInput: CreateTodoInput) {
-    return 'This action adds a new todo';
+  constructor(private prisma: PrismaService) {}
+  async create(createTodoInput: CreateTodoInput) {
+    return await this.prisma.todo.create({
+      data: { title: createTodoInput.title },
+    });
   }
 
   findAll() {
